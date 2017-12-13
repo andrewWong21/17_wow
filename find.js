b74c5e38c12e27685c61ae839a0e82ed7b7e4d2d
andrewWong21 <awong21@stuy.edu>
2017-12-13 14:45:59 -0500

    worked on findIt

--- find.js
+++ find.js
@@ -13,18 +13,31 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  return Math.sqrt((x1-x0)**2 + (y1-y0)**2);
+    var dist = Math.sqrt((x1-x0)**2 + (y1-y0)**2) / 5;
+    
+    if (dist > 100){
+       dist = 50;
+    }
+    else if (dist < 5){
+        dist = 5;
+    }
+    else if (dist < 100){
+        return dist / 2;
+    };
+    return dist;
 };
 
 
 var findIt = function(e) {
-	//return this.clientX;
-  box.style.backgroundColor = distance(this.clientX, this.clientY, targetX, targetY);
-};
+    var mouseDist = distance(e.clientX, e.clientY, targetX, targetY);
+    var lightness = 90 - mouseDist;
+    box.style.backgroundColor = 'hsl(25,100%,' + lightness + '%)';
+    console.log("dist: " + mouseDist);    
+    console.log("L: " + lightness);
+    };
 
 /*
 your OTHER FXNS
-
 */
 
 box.addEventListener("mousemove", findIt);
